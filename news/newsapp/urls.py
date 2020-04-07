8a67f1c3b0ae64b3659c87e3aea86837230a4b61
rian-uni <montgomeryrian@gmail.com>
2020-04-07 15:28:46 +0100

    Added templates

--- news/newsapp/urls.py
+++ news/newsapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('',views.index, name='index'),
+    path('<int:story_id>/', views.article, name='article'),
+
+
 ]
--- news/newsapp/views.py
+++ news/newsapp/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Story
+from django.template import loader
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    latest_story_list = Story.objects.order_by('-publication_date')[:5]
+    template = loader.get_template('newsapp/index.html')
+    context = {
+        'latest_story_list': latest_story_list,
+    }
+    return HttpResponse(template.render(context, request))
 
+def article(request, story_id):
+    return HttpResponse("You're looking at story " % story_id)

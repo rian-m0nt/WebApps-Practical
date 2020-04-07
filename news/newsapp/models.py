faad734d1e7374e3078c81a2bcd0a8667ceab830
rian-uni <montgomeryrian@gmail.com>
2020-04-07 16:00:57 +0100

    Added bootstrap for nicer styling

--- news/newsapp/models.py
+++ news/newsapp/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
 class Story(models.Model):
     story_title = models.CharField(max_length=255)
     publication_date = models.DateTimeField('Published On')
     story_text = models.CharField(max_length=500)
-
-
